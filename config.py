6eb1185fab4e2b94c18ba9746399ef2a2e01fd83
Alexander Galkovsky <31244229+prestante@users.noreply.github.com>
2023-03-09 18:20:58 +0100

    Delete config.py
    because openai deactivates any shared key

--- config.py
+++
@@ -1,3 +0,0 @@
-#TOKEN = "6179153423:AAEdOJCBo9oX7oDZVSws6RtdHHoMJEfdUJI"   # @Prestante_Bot
-TOKEN = "6297400628:AAGcE02FA9NO_wnYIAO6OfDPD1U77wNPJKs"    # @ChatGPT_Galk_Bot
-OPENAI_TOKEN = "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"  # obtained from https://platform.openai.com/account/api-keys
