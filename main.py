7041408b377ce9376a5e527d2a13f6f8cd9b919d
prestante <prestante@yandex.ru>
2023-07-04 11:24:51 +0300

    A lot of changes to entire script. Added anton to approved_users list. Changed script structure and edited comments.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ openai.api_key = config.OPENAI_TOKEN  # init openai
 bot = Bot(token=config.TOKEN)  # init aiogram
 dp = Dispatcher(bot)  # dispatcher bot
 conversation_history = {}  # dialog history
+approved_users = ['pres',379179502,'anton',984055351]
 
 
 def dt():
@@ -19,37 +20,51 @@ def dt():
 async def gpt_answer(message: types.Message):
     global conversation_history
 
-    # Вычленяем пользователя из сообщения и печатаем в консоль инфу о юзере, если он новый
+    # Вычленяем пользователя из сообщения
     user = message.from_user
+
+    # Если с юзером еще не было истории сообщений, пишем в консоли его данные
     if user.id not in conversation_history:
         print(f"[bold magenta]{dt()} - {user}[/bold magenta]")
 
-    # Не реагируем на команду /start
+    # Проверяем есть ли юзер в списке allowed_users
+    if user.id not in approved_users:
+        print(f"[bold red]{dt()} - User {user.id} is not in the allowed_users list[/bold red]")
+        return
+
+    # Игнорируем команду /start
     if message.text.lower() == "/start":
         return
 
     # Получаем историю диалога для текущего пользователя или создаем новую
     user_history = conversation_history.get(user.id, [])
 
-    # Формируем запрос к API OpenAI с использованием истории диалога текущего пользователя
+    # Получаем и пишем в консоль вопрос от пользователя
     question = message.text
     print(f"[white]{dt()} - [/white][green]{user.username}: [bold]{question}[/bold][green]")
+
+    # Обнуляем контекст если пришло сообщение о сбросе
     if question.lower() in ["сброс", "reset", "clear", "cls", "restart"]:
         conversation_history[user.id] = []
         answer = f"Context for {user.username} has been cleared"
         print(f"[white]{dt()} - {answer}[/white]")
         await message.answer(answer)
         return
-    user_history.append({"role": "user", "content": question})
-    answer = openai.ChatCompletion.create(
-        model="gpt-4", messages=user_history
-        #model="gpt-3.5-turbo", messages=user_history
-    ).choices[0].message.content
 
-    # Обновляем историю диалога для текущего пользователя
+    user_history.append({"role": "user", "content": question})
+
+    # Получаем ответ от API OpenAI
+    answer = openai.ChatCompletion.create(
+        #model="gpt-4", messages=user_history
+        model="gpt-3.5-turbo", messages=user_history
+    ).choices[0].message.content
     print(f"[white]{dt()} - [/white][cyan]ChatGPT: [bold]{answer}[/bold][cyan]")
     user_history.append({"role": "assistant", "content": answer})
+
+    # Обновляем историю диалога для текущего пользователя
     conversation_history[user.id] = user_history
+
+    # Отправляем ответ в чат пользователю
     await message.answer(answer)
 
 # run long-polling
