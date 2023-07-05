dfccadd315ea6b2df2be9c048f36044a1ed4f696
prestante <prestante@yandex.ru>
2023-07-05 17:31:42 +0300

    Added a mechanism to auto-clear the most of conversation history after it reaches 3000+ tokens.

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 # Telegram Bot ChatGPT with history (context)
 from aiogram import Bot, Dispatcher, executor, types
-from rich import print
 from datetime import datetime
+from rich import print
 import config
 import openai
+import tiktoken
 
 openai.api_key = config.OPENAI_TOKEN  # init openai
 bot = Bot(token=config.TOKEN)  # init aiogram
 dp = Dispatcher(bot)  # dispatcher bot
 conversation_history = {}  # dialog history
-approved_users = ['pres',379179502,'anton',984055351]
+approved_users = ['pres', 379179502, 'anton', 984055351]
+model = "gpt-3.5-turbo"
 
 
 def dt():
     return datetime.now().isoformat(timespec='milliseconds', sep=' ')
+
+
+def count_tokens(conversation_history) -> int:
+    encoding = tiktoken.encoding_for_model(model)
+    token_count = 0
+    for message in conversation_history:
+        content = message['content']
+        token_count += len(encoding.encode(content))
+    return token_count
 
 
 @dp.message_handler()
@@ -41,7 +52,7 @@ async def gpt_answer(message: types.Message):
 
     # Получаем и пишем в консоль вопрос от пользователя
     question = message.text
-    print(f"[white]{dt()} - [/white][green]{user.username}: [bold]{question}[/bold][green]")
+    #print(f"[white]{dt()} - [/white][green]{user.username}: [bold]{question}[/bold][/green]")
 
     # Обнуляем контекст если пришло сообщение о сбросе
     if question.lower() in ["сброс", "reset", "clear", "cls", "restart"]:
@@ -55,10 +66,9 @@ async def gpt_answer(message: types.Message):
 
     # Получаем ответ от API OpenAI
     answer = openai.ChatCompletion.create(
-        #model="gpt-4", messages=user_history
-        model="gpt-3.5-turbo", messages=user_history
+        model=model, messages=user_history
     ).choices[0].message.content
-    print(f"[white]{dt()} - [/white][cyan]ChatGPT: [bold]{answer}[/bold][cyan]")
+    #print(f"[white]{dt()} - [/white][cyan]ChatGPT: [bold]{answer}[/bold][/cyan]")
     user_history.append({"role": "assistant", "content": answer})
 
     # Обновляем историю диалога для текущего пользователя
@@ -66,6 +76,14 @@ async def gpt_answer(message: types.Message):
 
     # Отправляем ответ в чат пользователю
     await message.answer(answer)
+
+    if count_tokens(conversation_history[user.id]) > 3000:
+        clear_message = "<b><i>Conversation history is too big, clearing...</i></b>"
+        await message.answer(clear_message, parse_mode=types.ParseMode.HTML)
+        print(f"[black]{dt()}[/black][gray] - Conversation history is too big, clearing...[/gray]")
+        conversation_history[user.id] = conversation_history[user.id][-4:]
+
+    print(conversation_history[user.id])
 
 # run long-polling
 if __name__ == "__main__":
