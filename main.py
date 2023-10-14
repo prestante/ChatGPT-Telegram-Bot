9116f2ea951215b4fe04c82d9d563f8416991ccf
prestante <prestante@yandex.ru>
2023-10-14 11:54:46 +0200

    Rewritten entire program to work with aiogram library v3+

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 # Telegram Bot ChatGPT with history (context)
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+
 from datetime import datetime
 from rich import print
-import config
 import openai
 import tiktoken
+import asyncio
 import logging
+import sys
+from os import getenv
 
 logging.basicConfig(level=logging.FATAL)  # Set up logging to avoid unnecessary Tracebacks
-openai.api_key = config.OPENAI_TOKEN  # init openai
-bot = Bot(token=config.TOKEN)  # init aiogram
-dp = Dispatcher(bot)  # dispatcher bot
+openai.api_key = getenv('OpenAi')  # init openai
+dp = Dispatcher()  # dispatcher bot
 conversation_history = {}  # dialog history
 approved_users = ['Pres', 379179502, 'Anton', 984055351, 'Julia', 406186116, 'Anna', 402718700]
 users_16k = ['Anna', 402718700]  # users who will use 16k context model
@@ -32,8 +38,21 @@ def count_tokens(conversation_history) -> int:
     return token_count
 
 
-@dp.message_handler()
-async def gpt_answer(message: types.Message):
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+
+
+@dp.message()
+async def gpt_answer(message: types.Message) -> None:
     global conversation_history
 
     # Getting the user data from received message
@@ -98,6 +117,13 @@ async def gpt_answer(message: types.Message):
         conversation_history[user.id] = conversation_history[user.id][-4:]
 
 
-# Run long-polling
+async def main() -> None:
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(token=getenv('ChatGPT_Galk_Bot'), parse_mode=ParseMode.HTML)
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
